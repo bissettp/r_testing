f4d21c665c4166b7b3d0a88cf61e92b93c853044
Patrick Bissett <pbissett@stanford.edu>
2016-05-10 13:05:58 -0700

    fixed wrong age issue

--- demean_age.R
+++ demean_age.R
@@ -2,9 +2,8 @@ args <- commandArgs(trailingOnly = TRUE)
 
 demographics <- read.csv(file=args[1], head=TRUE, sep="\t")
 
-age <- demographics[4]
+age <- demographics$age
 
-stopifnot(mean_age<-sum(age)/length(age))
 mean_age <- sum(age)/length(age)
 stopifnot(mean_age < 100)
 stopifnot(mean_age > 10)
